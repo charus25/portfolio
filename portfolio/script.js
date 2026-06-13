34517b912c628ca0031194b48ead69245fa2453f
charus25 <164998624+charus25@users.noreply.github.com>
2026-06-13 19:19:10 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 50) {
+    header.style.boxShadow = "0 5px 20px rgba(0,0,0,0.3)";
+  } else {
+    header.style.boxShadow = "none";
+  }
+});
